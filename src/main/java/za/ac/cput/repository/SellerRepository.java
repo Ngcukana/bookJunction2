79c3b8932eba6de79ec20ca9d2fa739dfd27e1e0
Mkhetyeva <127616754+Mkhetyeva@users.noreply.github.com>
2024-05-17 18:47:13 +0200

    update

--- src/main/java/za/ac/cput/repository/SellerRepository.java
+++ src/main/java/za/ac/cput/repository/SellerRepository.java
@@ -1,38 +1,44 @@
 package za.ac.cput.repository;
 
-
 import za.ac.cput.domain.Seller;
 
 import java.util.ArrayList;
 import java.util.List;
 
-    public class SellerRepository {
-        private List<Seller> sellers;
+public class SellerRepository implements ISellerRepository {
+    private List<Seller> sellers;
 
-        public SellerRepository() {
-            this.sellers = new ArrayList<>();
-        }
-
-        public void addSeller(Seller seller) {
-            sellers.add(seller);
-        }
-
-        public void removeSeller(Seller seller) {
-            sellers.remove(seller);
-        }
-
-        public List<Seller> getAllSellers() {
-            return new ArrayList<>(sellers);
-        }
-
-        public Seller findSellerById(String email) {
-            for (Seller seller : sellers) {
-                if (seller.getEmail() == email) {
-                    return seller;
-                }
-            }
-            return null;
-        }
+    public SellerRepository() {
+        this.sellers = new ArrayList<>();
     }
 
+    @Override
+    public void addSeller(Seller seller) {
+        sellers.add(seller);
+    }
 
+    @Override
+    public void removeSeller(Seller seller) {
+        sellers.remove(seller);
+    }
+
+    @Override
+    public List<Seller> getAllSellers() {
+        return new ArrayList<>(sellers);
+    }
+
+    @Override
+    public Seller findSellerByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public Seller findSellerById(String Email) {
+        for (Seller seller : sellers) {
+            if (seller.getEmail() == Email) {
+                return seller;
+            }
+        }
+        return null;
+    }
+}
