fb5ed4f3c91e4209e3404c12b3fb5d4544ffa874
Breezieee <221427953@mycput.ac.za>
2024-05-17 23:46:27 +0200

    My work

---
+++ src/test/java/za/ac/cput/factory/RatingFactoryTest.java
@@ -0,0 +1,14 @@
+package za.ac.cput.factory;
+
+public class RatingFactoryTest {
+
+    public void testCreateBuyerFactory() {
+
+
+        String reviewID = "User23665";
+        String firstName = "Busi";
+        String lastName = "Mthembu";
+        String review = "The book is in good condition ";
+
+    }
+}
