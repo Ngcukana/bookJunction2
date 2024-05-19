bc926d3528ef317a799fab4cf17947326bd3203a
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-05-19 12:33:41 +0200

    introducing the service package

--- src/main/java/za/ac/cput/repository/BookRepository.java
+++ src/main/java/za/ac/cput/repository/BookRepository.java
@@ -2,56 +2,8 @@ package za.ac.cput.repository;
 
 import za.ac.cput.domain.Book;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public class BookRepository implements IBookRepository{
-    private static BookRepository repository = null;
-    private List<Book> BookList;
-    private BookRepository() {
-        BookList = new ArrayList<>();
-    }
-
-    public static IBookRepository getRepository() {
-        if (repository == null) {
-            repository = new BookRepository();
-        }
-        return repository;
-    }
-
-    @Override
-    public List<Book> getAll() {
-        return BookList;
-    }
-
-    @Override
-    public Book create(Book object) {
-        if (BookList.add(object)) {
-            return object;
-        }
-        return null;
-    }
-
-    @Override
-    public Book read(Long aLong) {
-        for (Book book : BookList) {
-            if (Objects.equals(book.getBookID(), book)) {
-            }
-            return book;
-        }
-        return null;
-    }
-
-    @Override
-    public Book update(Book object) {
-        Book existingBook = read(object.getBookID());
-        if (existingBook != null) {
-            BookList.remove(existingBook);
-            BookList.add(object);
-            return object;
-        }
-        return null;
-    }
-
+public interface BookRepository extends IRepository<Book, Long> {
+    List<Book> getAll();
 }
---
+++ src/main/java/za/ac/cput/service/IService.java
@@ -0,0 +1,7 @@
+package za.ac.cput.service;
+
+public interface IService <Object, ID>{
+    Object create(Object obj);
+    Object read(ID id);
+    Object update(Object obj);
+}
