7e4c5897d3dde3f3d8c87a307309109d4f1cb31e
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-05-26 17:00:09 +0200

    Completed my BookController class

--- src/main/java/za/ac/cput/controller/BookController.java
+++ src/main/java/za/ac/cput/controller/BookController.java
@@ -1,4 +1,29 @@
 package za.ac.cput.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Book;
+import za.ac.cput.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Book")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/create")
+    public Book create(@RequestBody Book book){ return bookService.create(book);}
+
+    @GetMapping("/read/{title}")
+    public Book read(@PathVariable String title){return bookService.read(title);}
+
+    @PostMapping("/update")
+    public Book update(@RequestBody Book book){return bookService.update(book);}
+
+    @GetMapping("/getall")
+    public List<Book> getall(){return bookService.getall();}
+
 }
